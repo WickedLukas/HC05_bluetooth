868eed3db7bbe38eddcf98de7b76a36f1a705daf
Lukas <lukas_m@gmx.de>
2017-12-23 10:57:09 +0100

    Restructure, clean up and compile test.

--- bluetooth_test/bluetooth_test.ino
+++ bluetooth_test/bluetooth_test.ino
@@ -6,23 +6,24 @@ void setup()
   Serial.begin(115200);
   while (!Serial); // wait for Leonardo eNUMeration, others continue immediately
 
-  // // initialize serial1 communication
+  // initialize serial1 communication
   Serial1.begin(115200);
   while (!Serial1); // wait for Leonardo eNUMeration, others continue immediately
 }
 
 void loop()
 {
-  while(Serial1.available())
-  {//while there is data available on the serial monitor
+  // while there is data available on the serial monitor
+  while(Serial1.available()) {
     message+=char(Serial1.read());//store string from serial command
   }
-  if(!Serial1.available())
-  {
-    if(message!="")
-    {//if data is available
-      Serial.println(message); //show the data
-      message=""; //clear the data
+  if(!Serial1.available()) {
+    // if data is available
+    if(message!="") {
+      // show data
+      Serial.println(message);
+      // clear data
+      message="";
     }
   }
   delay(1000); //delay
